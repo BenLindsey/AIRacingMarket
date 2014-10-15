f6479b12f654542bb27509670dce484311daede3
Ben <bl2312@imperial.ac.uk>
2014-10-15 17:48:48 +0100

    Fixed url

--- AIRacingNode/routes/index.js
+++ AIRacingNode/routes/index.js
@@ -55,7 +55,7 @@ router.post('/script', function(req, res) {
             res.send("There was a problem adding the information to the database.");
         }
         else {
-            var url = "/WebBuild.html?url=" + scriptName;
+            var url = "/WebBuild.html?url=http://146.169.47.15:3000/script/" + scriptName;
             // If it worked, set the header so the address bar doesn't still say /adduser
             res.location(url);
             // And forward to success page
