b30b13b1aa745a8cc70cc77be1b9fd0c889d9984
Ben Lindsey <ben.lindsey12@imperial.ac.uk>
2014-10-16 14:57:15 +0100

    Fixed redirect

--- AIRacingNode/routes/login.js
+++ AIRacingNode/routes/login.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res) {
     res.render('login', { message: req.flash('loginMessage') });
 });
 
+router.post('/', function(req, res) {
+  return req.passport.authenticate('local-login', {
+      successRedirect: '/leaderboard',
+      failureRedirect: '/login', // redirect back to the signup page if there is an error
+      failureFlash: true // allow flash messages
+    })
+});
+
 module.exports = router;
--- AIRacingNode/routes/signup.js
+++ AIRacingNode/routes/signup.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res) {
 // process the signup form
 router.post('/', function(req, res) {
     return req.passport.authenticate('local-signup', {
-        successRedirect : '/profile', // redirect to the secure profile section
+        successRedirect : '/leaderboard',
         failureRedirect : '/signup', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     });
