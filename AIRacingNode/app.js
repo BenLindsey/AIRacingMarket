8e715bd9cea6cd17940e30776f23c30c44c5ad28
Ben Lindsey <ben.lindsey12@imperial.ac.uk>
2014-10-16 14:46:40 +0100

    Added signup

--- AIRacingNode/app.js
+++ AIRacingNode/app.js
@@ -96,12 +96,12 @@ app.use(function(err, req, res, next) {
     });
 });
 
-app.get('/', function(req, res) {
+app.get('/signup', function(req, res) {
     res.render('signup', {  title: 'Register' , message: req.flash('signupMessage') })
 });
 
 // process the signup form
-app.post('/', passport.authenticate('local-signup', {
+app.post('/signup', passport.authenticate('local-signup', {
     successRedirect : '/profile', // redirect to the secure profile section
     failureRedirect : '/signup', // redirect back to the signup page if there is an error
     failureFlash : true // allow flash messages
