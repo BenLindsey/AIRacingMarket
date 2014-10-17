e03c4c884accf1cd8f8ad34ed07658d7f44dbdbc
Ben Lindsey <ben.lindsey12@imperial.ac.uk>
2014-10-17 15:50:13 +0100

    Switched time to mongoose

---
+++ AIRacingNode/models/time.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+// define the schema for our user model
+var timeSchema = mongoose.Schema({
+    time  : {
+        username     : String,
+        scriptName   : String,
+        scriptTime   : Double
+    }
+});
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('Time', timeSchema);
--- AIRacingNode/routes/time.js
+++ AIRacingNode/routes/time.js
@@ -1,29 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+var Time = require('../models/time');
+
 router.post('/', function(req, res) {
-    // Set our internal DB variable
-    var db = req.db;
+    var newTime = new Time();
 
-    // Get our form values. These rely on the "name" attributes
-    var userName = req.body.username;
-    var scriptName = req.body.scriptname;
-    var scriptTime = parseFloat(req.body.time);
+    // set the user's local req.body.username
+    newTime.time.username = req.body.username;
+    newTime.time.scriptName = req.body.scriptname;;
+    newTime.time.scriptTime = parseFloat(req.body.time);;
 
-    // Set our collection
-    var collection = db.get('timecollection');
-
-    // Submit to the DB
-    collection.insert({
-        "username": userName,
-        "scriptName": scriptName,
-        "scriptTime": scriptTime
-    }, function (err, doc) {
+    newTime.save(function(err) {
         if (err) {
             // If it failed, return error
             res.send("There was a problem adding the information to the database.");
-        }
-        else {
+        } else {
             // If it worked, set the header so the address bar doesn't still say /adduser
             res.location("leaderboard");
             // And forward to success page
