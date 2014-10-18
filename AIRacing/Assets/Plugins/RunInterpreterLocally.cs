1a4a6932226da5da33c2a9e1bf14fd5a842a3f18
Finlay Curran <finlay.curran@gmail.com>
2014-10-18 14:57:24 +0100

    Cars can now run any javascript files

---
+++ AIRacing/Assets/Plugins/RunInterpreterLocally.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class RunInterpreterLocally : MonoBehaviour {
+
+    public ScriptInterpreter interpreter;
+    public string file = "racetrack.txt";
+    private string folder = Directory.GetCurrentDirectory() + "\\Assets\\Racing Scripts\\";
+
+	// Use this for initialization
+	void Start () {
+        string path = folder + file;
+        if (File.Exists(path)) {
+            Debug.Log("Setting local script path to: " + path);
+            interpreter.SetLocalScriptPath(path);
+        }
+        else {
+            Debug.Log("Path: '" + path + "' does not exist.");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ AIRacing/Assets/Wrapper.js
@@ -0,0 +1,42 @@
+﻿#pragma strict
+
+var api : AiApi;
+
+var URL = "http://146.169.47.15:3000/";
+
+var scriptName;
+var scriptPath;
+
+var scriptContents;
+
+var www : WWW;
+
+function Start () {
+	api = GetComponentInChildren(AiApi);
+	
+	Application.ExternalCall("SetScript", "");
+}
+
+function Update () {
+	if (www != null && www.isDone) {
+		eval(www.text);
+	}
+}
+
+function SetScriptName(name) {
+
+	scriptName = name;
+
+	Debug.Log("Script name set: " + scriptName);
+
+	scriptPath = URL + "script/" + scriptName;
+	Debug.Log("Path set: " + scriptPath);
+
+	www = new WWW(scriptPath);
+}
+
+function SetLocalScriptPath(scriptPath) {
+	scriptPath = "file:///" + scriptPath;
+	www = new WWW(scriptPath);
+	Debug.Log("Local path set: " + scriptPath);
+}
