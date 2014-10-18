6874c36cae4013e1e4fb68d3930bab297b38e917
Christophe Steininger <christophe.steinininger@gmail.com>
2014-10-18 19:23:45 +0100

    Move wheel control logic to the car script and rotate all wheels when moving.

--- AIRacing/Assets/OurCar.cs
+++ AIRacing/Assets/OurCar.cs
@@ -5,38 +5,71 @@ public class OurCar : MonoBehaviour {
 
 	public Transform centerOfMass;
 
-	public WheelControl rightWheel;
-	public WheelControl leftWheel;
+    public WheelCollider frontLeft;
+    public WheelCollider frontRight;
+    public WheelCollider rearLeft;
+    public WheelCollider rearRight;
+
+    public Transform frontLeftTransform;
+    public Transform frontRightTransform;
+    public Transform rearLeftTransform;
+    public Transform rearRightTransform;
+    private Transform[] steeringTransforms;
+    private Transform[] allTransforms;
+
+    private float throttle;
+    private float steer;
 
 	public Material brakeLights;
 
 	// Use this for initialization
 	void Start () {
+
 		rigidbody.centerOfMass = centerOfMass.localPosition;
+
+        steeringTransforms = new Transform[] { frontLeftTransform,
+            frontRightTransform };
+        allTransforms = new Transform[] { frontRightTransform,
+            frontRightTransform, rearLeftTransform, rearRightTransform };
 	}
 
 	// Update is called once per frame
-	void FixedUpdate () {
-	}
+    void FixedUpdate() {
+
+        if (throttle < 0) {
+            brakeLights.SetFloat("_Intensity", Mathf.Abs(throttle / 100));
+        }
+        else {
+            brakeLights.SetFloat("_Intensity", 0);
+        }
+
+        // Set the power of the front wheels.
+        frontLeft.motorTorque = throttle;
+        frontRight.motorTorque = throttle;
+
+        // Steer the front wheels.
+        frontLeft.steerAngle = steer;
+        frontRight.steerAngle = steer;
+
+        // Rotate the front wheels around the y axis to show steering.
+        frontLeftTransform.localEulerAngles = new Vector3(frontLeftTransform.localEulerAngles.x,
+            frontLeft.steerAngle, frontLeftTransform.localEulerAngles.z);
+        frontRightTransform.localEulerAngles = new Vector3(frontRightTransform.localEulerAngles.x,
+            frontRight.steerAngle, frontRightTransform.localEulerAngles.z);
+
+        // Cumulatively rotate the wheel around the x axis to show speed.
+        // Magic number: rpm / 60 * 360 * fixedDeltaTime.
+        frontLeftTransform.Rotate(frontLeft.rpm * 6 * Time.fixedDeltaTime, 0, 0);
+        frontRightTransform.Rotate(frontRight.rpm * 6 * Time.fixedDeltaTime, 0, 0);
+        rearLeftTransform.Rotate(rearLeft.rpm * 6 * Time.fixedDeltaTime, 0, 0);
+        rearRightTransform.Rotate(rearRight.rpm * 6 * Time.fixedDeltaTime, 0, 0);
+    }
 
 	public void SetThrottle(float value) {
-
-		if (value < 0) {
-			brakeLights.SetFloat("_Intensity", Mathf.Abs(value));
-		} else {
-			brakeLights.SetFloat("_Intensity", 0);
-		}
-
-		float wheelSpeed = value * 100;
-
-		leftWheel.SetWheelSpeed(wheelSpeed);
-		rightWheel.SetWheelSpeed(wheelSpeed);
+        throttle = value * 100;
 	}
 
 	public void SetSteer(float value) {
-		value = Mathf.Clamp(value, -45, 45);
-
-		leftWheel.SetTurnAngle(value);
-		rightWheel.SetTurnAngle(value);
+        steer = Mathf.Clamp(value, -45, 45);
 	}
 }
