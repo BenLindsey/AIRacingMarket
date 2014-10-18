c9b3d7f3ba73db128d19b0dba5c46dc357a77600
Christophe Steininger <christophe.steinininger@gmail.com>
2014-10-18 18:56:24 +0100

    Add wheel rotation when moving.

--- AIRacing/Assets/WheelControl.cs
+++ AIRacing/Assets/WheelControl.cs
@@ -3,33 +3,25 @@ using System.Collections;
 
 public class WheelControl : MonoBehaviour {
 
-	float steerAngle;
-
-	Transform car;
-
 	public WheelCollider wheelCollider;
 
 	// Use this for initialization
 	void Start () {
-		car = transform.parent;
 	}
 
 	public void FixedUpdate () {
-		float currentAngle =  Vector3.Angle(car.forward, transform.forward);
 
-		if (transform.forward.x < car.forward.x) {
-			currentAngle = -currentAngle;
-		}
+        // Rotate the the wheel around the y axis to show steering.
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x,
+            wheelCollider.steerAngle, transform.localEulerAngles.z);
 
-		//Debug.Log("Steer: " + steerAngle);
-
-        wheelCollider.steerAngle = steerAngle;
-
-        rigidbody.MoveRotation(Quaternion.AngleAxis(steerAngle, car.up) * car.rotation);
+        // Cumulatively rotate the wheel around the x axis to show speed.
+        // Magic number: rpm / 60 * 360 * fixedDeltaTime.
+        transform.Rotate(wheelCollider.rpm * 6 * Time.fixedDeltaTime, 0, 0);
 	}
-	
-	public void SetTurnAngle(float value) {
-		steerAngle = value;
+
+    public void SetTurnAngle(float value) {
+        wheelCollider.steerAngle = value;
 	}
 
 	public void SetWheelSpeed(float value) {
