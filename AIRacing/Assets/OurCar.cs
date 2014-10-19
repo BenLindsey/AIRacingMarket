3b734bdb4c5d9f13a042113374aed3faae3bd4b2
Christophe Steininger <christophe.steinininger@gmail.com>
2014-10-19 18:53:56 +0100

    Car does not flip as easily.
    Add an anti roll bar to both axles, reduce the maximum steering angle at
    high speeds and modify wheel suspension and sideways friction. The anti
    roll bars work well, but only with a high (1 meter) suspension.

--- AIRacing/Assets/OurCar.cs
+++ AIRacing/Assets/OurCar.cs
@@ -12,10 +12,20 @@ public class OurCar : MonoBehaviour {
 
 	public Transform centerOfMass;
 
+    // Wheel meshes.
     public Transform frontLeftTransform;
     public Transform frontRightTransform;
     public Transform rearLeftTransform;
     public Transform rearRightTransform;
+
+    public AntiRollBar frontAntiRoll;
+    public AntiRollBar rearAntiRoll;
+
+    // Physics variables.
+    public float downwardsForce = 0;
+    public float suspensionDistance = 0.1f;
+    public float suspensionFrontForce = 18500;
+    public float suspensionRearForce = 9000;
 
     private Wheel[] wheels;
 
@@ -31,6 +41,14 @@ public class OurCar : MonoBehaviour {
 		rigidbody.centerOfMass = centerOfMass.localPosition;
 
         SetupWheels();
+
+        frontAntiRoll.leftWheel = wheels[0].collider;
+        frontAntiRoll.rightWheel = wheels[1].collider;
+        frontAntiRoll.antiRoll = suspensionFrontForce;
+
+        rearAntiRoll.leftWheel = wheels[2].collider;
+        rearAntiRoll.rightWheel = wheels[3].collider;
+        rearAntiRoll.antiRoll = suspensionRearForce;
 	}
 
 	// Update is called once per frame
@@ -49,30 +67,53 @@ public class OurCar : MonoBehaviour {
     private void SetupWheels() {
 
         wheels = new Wheel[4];
-        wheels[0] = SetupWheel(frontLeftTransform, true, true);
-        wheels[1] = SetupWheel(frontRightTransform, true, true);
-        wheels[2] = SetupWheel(rearLeftTransform, false, false);
-        wheels[3] = SetupWheel(rearRightTransform, false, false);
+
+        bool frontWheelDrive = true;
+        bool rearWheelDrive = !frontWheelDrive; // Both can be set to true.
+
+        wheels[0] = SetupWheel(frontLeftTransform, frontWheelDrive, true);
+        wheels[1] = SetupWheel(frontRightTransform, frontWheelDrive, true);
+        wheels[2] = SetupWheel(rearLeftTransform, rearWheelDrive, false);
+        wheels[3] = SetupWheel(rearRightTransform, rearWheelDrive, false);
     }
 
     // Creates the wheel object and its collider from the transform.
-    private Wheel SetupWheel(Transform wheelTransfrom, bool canSteer,
-        bool isPowered) {
+    private Wheel SetupWheel(Transform wheelTransfrom, bool isPowered, bool isFront) {
 
         GameObject colliderObject = new GameObject(wheelTransfrom.name + " Collider");
         colliderObject.transform.position = wheelTransfrom.position;
-        colliderObject.transform.parent = wheelTransfrom.parent;
+        colliderObject.transform.parent = wheelTransfrom.parent.parent;
         colliderObject.transform.rotation = wheelTransfrom.rotation;
 
+        // Create the collider and set the wheel's radius, friction and suspension.
         WheelCollider collider = colliderObject.AddComponent<WheelCollider>();
         collider.radius = wheelTransfrom.GetComponentsInChildren<Transform>()[1]
             .renderer.bounds.size.y / 2;
+        collider.steerAngle = 0;
+
+        WheelFrictionCurve frictionCurve = new WheelFrictionCurve();
+        frictionCurve.extremumSlip = 1.0f;  // Default values apart from stiffness.
+        frictionCurve.extremumValue = 20000.0f;
+        frictionCurve.asymptoteSlip = 2.0f;
+        frictionCurve.asymptoteValue = 10000.0f;
+        frictionCurve.stiffness = 0.04f;
+        collider.sidewaysFriction = frictionCurve;
+
+        JointSpring suspension = new JointSpring();
+        suspension.spring = (isFront) ? suspensionFrontForce : suspensionRearForce;
+        suspension.damper = 50;
+        suspension.targetPosition = 0;
+        collider.suspensionSpring = suspension;
+        collider.suspensionDistance = suspensionDistance;
 
         Wheel wheel = new Wheel();
         wheel.collider = collider;
         wheel.transform = wheelTransfrom;
-        wheel.canSteer = canSteer;
+        wheel.canSteer = isFront;
         wheel.isPowered = isPowered;
+
+        // Move the wheel down according to the height of the suspension.
+        wheelTransfrom.position += Vector3.down * suspensionDistance;
 
         return wheel;
     }
@@ -102,6 +143,12 @@ public class OurCar : MonoBehaviour {
 		// Cumulatively rotate the wheel around the x axis to show speed.
 		// Magic number: rpm / 60 * 360 * fixedDeltaTime.
 		wheel.transform.Rotate(wheel.collider.rpm * 6 * Time.fixedDeltaTime, 0, 0);
+
+        // TODO: Update the position of the wheel according to the suspension.
+
+        // Messy hack, force the car down on the road to reduce flips.
+        rigidbody.AddForceAtPosition(Vector3.down * downwardsForce,
+            centerOfMass.position);
     }
 
     public void SetThrottle(float value) {
@@ -109,10 +156,25 @@ public class OurCar : MonoBehaviour {
 	}
 
 	public void SetSteer(float value) {
-        steer = Mathf.Clamp(value, -45, 45);
+        steer = Mathf.Clamp(value, -45, 45) / 45
+            * GetMaxSteeringAngle(rigidbody.velocity.magnitude);
 	}
 
 	public void SetBrake(float value) {
 		brake = Mathf.Max(value, 0);
 	}
+    
+    private float GetMaxSteeringAngle(float speed) {
+
+        float topSpeed = 35;
+        float minimumTurn = 15;
+        float maximumTurn = 45;
+
+	    if (speed > topSpeed / 2) {
+		    return minimumTurn;
+        }
+	
+	    float speedIndex = 1 - (speed / (topSpeed / 2));
+	    return minimumTurn + speedIndex * (maximumTurn - minimumTurn);
+    }
 }
