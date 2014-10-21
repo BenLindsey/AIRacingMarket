242ef87c0023a408d1b6fa6870d6fa16e971a40c
Ben Lindsey <benlindsey@outlook.com>
2014-10-21 16:41:56 +0100

    Added logging for redirect on post script

--- AIRacingNode/routes/script.js
+++ AIRacingNode/routes/script.js
@@ -33,6 +33,8 @@ router.post('/', isLoggedIn, function(req, res) {
         else {
             // Build the inputs to unity
             var url = "/WebBuild.html?" + require('qs').stringify({scriptname : req.body.scriptname, levelname : req.body.levelname});
+
+            console.log("Redirecting user to: " + url);
             // If it worked, set the header so the address bar doesn't still say /script
             res.location(url);
             // And forward to success page
