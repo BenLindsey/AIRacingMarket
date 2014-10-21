7100bc38d37df78e2f3b4a4997d3ff6b70a17aec
Ben Lindsey <benlindsey@outlook.com>
2014-10-21 16:21:54 +0100

    Removing grouping code, possibly broken.

--- AIRacingNode/routes/leaderboard.js
+++ AIRacingNode/routes/leaderboard.js
@@ -3,9 +3,9 @@ var router = express.Router();
 
 /* GET Times */
 router.get('/', function(req, res) {
-    var db = req.db;
-    var collection = db.get('timecollection');
-    collection.group("levelname").find({}, {sort : { time : 1 }}, function(e, docs) {
+    var collection = req.db.get('timecollection');
+
+    collection.find({}, {sort : { time : 1 }}, function(e, docs) {
         res.render('leaderboard', {
             "leaderboard" : docs
         });
