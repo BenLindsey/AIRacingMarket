3334f6111032244e4dbe206e61586577e23199ed
Ben Lindsey <ben.lindsey12@imperial.ac.uk>
2014-10-29 18:34:44 +0000

    Added post for admin to redirect to unity

--- AIRacingNode/routes/admin.js
+++ AIRacingNode/routes/admin.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET Times */
-router.get('/', function(req, res) {
+/* GET tournament form */
+router.get('/', isLoggedIn, function(req, res) {
     var collection = req.db.get('scriptcollection');
 
     collection.find({}, {sort : { scriptName : 1 }}, function(e, docs) {
@@ -10,6 +10,22 @@ router.get('/', function(req, res) {
             "scripts" : docs
         });
     });
+});
+
+/* POST to start tournament */
+router.post('/', isLoggedIn, function(req, res) {
+    // Build the inputs to unity
+    var url = "/WebBuild.html?scriptnameA=" + req.body.scriptnameA
+        +"&scriptnameB=" + req.body.scriptnameB;
+        +"&scriptnameC=" + req.body.scriptnameC;
+        +"&scriptnameD=" + req.body.scriptnameD;
+        +"&levelname=" + req.body.levelname;
+
+    console.log("Redirecting user to: " + url);
+    // If it worked, set the header so the address bar doesn't still say /script
+    res.location(url);
+    // And forward to success page
+    res.redirect(url);
 });
 
 module.exports = router;
