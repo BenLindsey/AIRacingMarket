806e421e784895cf6df942af195a35fc929c3d03
Thomas Kaplan <thomas.m.kaplan@gmail.com>
2014-10-29 14:42:28 +0000

    Revert port to out of range, cross your fingers

--- AIRacingNode/test/login_tests.js
+++ AIRacingNode/test/login_tests.js
@@ -2,49 +2,41 @@ var Browser = require("zombie");
 var assert = require("assert");
 var app = require("../app");
 
-var monk = require('monk');
-var db = monk(require('../config/database').url);
+describe('login page', function() {
 
-var branch = process.env.BRANCH;
+  before(function() {
+    server = app.listen(2999);
+  });
 
-require('../config/branch')(db, branch, function(port) {
-  describe('login page', function() {
+  beforeEach(function() {
+    Browser.localhost('146.169.47.15', 2999);
+    browser = Browser.create();
+  });
 
-    before(function() {
-      server = app.listen(port);
-    });
+  it('should visit the login page', function(done) {
+    browser.visit('/login', function() {
+      assert.equal(browser.location.pathname, "/login");
+      done();
+    })
+  });
 
-    beforeEach(function() {
-      Browser.localhost('146.169.47.15', port);
-      browser = Browser.create();
-    });
-
-    it('should visit the login page', function(done) {
-      browser.visit('/login', function() {
-        assert.equal(browser.location.pathname, "/login");
+  it('should redirect to home on login', function(done) {
+    browser.visit('/login')
+      .then(function() {
+        browser.fill("email", "a@b.com");
+        browser.fill("password", "c");
+        return browser.pressButton("Submit");
+      })
+      .done(function() {
+        browser.assert.success();
+        assert.equal(browser.location.pathname, "/");
+        browser.assert.text("title", "AI Racing Market");
         done();
       })
-    });
-
-    it('should redirect to home on login', function(done) {
-      browser.visit('/login')
-        .then(function() {
-          browser.fill("email", "a@b.com");
-          browser.fill("password", "c");
-          return browser.pressButton("Submit");
-        })
-        .done(function() {
-          browser.assert.success();
-          assert.equal(browser.location.pathname, "/");
-          browser.assert.text("title", "AI Racing Market");
-          done();
-        })
-    });
-
-    after(function() {
-      server.close();
-    });
-
   });
-});
 
+  after(function() {
+    server.close();
+  });
+
+});
