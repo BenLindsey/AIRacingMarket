40d3ba87ab9401bc1816ce7f7e1dda4f85bb4d3f
fcexb <finlay.curran@gmail.com>
2014-10-29 16:57:13 +0000

    Unity and api improvments

--- AIRacing/Assets/Plugins/AiApi.cs
+++ AIRacing/Assets/Plugins/AiApi.cs
@@ -3,7 +3,10 @@ using System.Collections;
 
 public class AiApi : MonoBehaviour {
 
-	public LayerMask ignoreLayers;
+    public LayerMask walls;
+    public LayerMask cars;
+
+    public BezierSpline middleLane;
 	Transform detector;
 
 	OurCar car;
@@ -14,17 +17,93 @@ public class AiApi : MonoBehaviour {
 		detector = transform.FindChild("Detector");
 	}
 
-	public float GetDistanceToNearestObstacle(float deg) {
+    public void SteerToMiddle() {
+        float rightDistance = GetDistanceToEdge(40);
+        float leftDistance = GetDistanceToEdge(-40);
+
+        if (rightDistance < 0 || leftDistance < 0) {
+            return;
+        }
+
+        float position = 2 * (rightDistance - leftDistance) / (rightDistance + leftDistance);
+
+        SetSteer(position * 25f);
+    }
+
+    public void SteerToLeft() {
+        float rightDistance = GetDistanceToEdge(40);
+        float leftDistance = GetDistanceToEdge(-40);
+
+        if (rightDistance < 0 || leftDistance < 0) {
+            return;
+        }
+
+        float position = 2 * (rightDistance - leftDistance) / (rightDistance + leftDistance);
+
+        SetSteer((position - 0.8f) * 25f);
+    }
+
+    public void SteerToRight() {
+        float rightDistance = GetDistanceToEdge(40);
+        float leftDistance = GetDistanceToEdge(-40);
+
+        if (rightDistance < 0 || leftDistance < 0) {
+            return;
+        }
+
+        float position = 2 * (rightDistance - leftDistance) / (rightDistance + leftDistance);
+
+        SetSteer((position + 0.8f) * 25f);
+    }
+
+	public float GetDistanceToEdge(float deg) {
 
 		Vector3 direction = Quaternion.AngleAxis(deg, transform.up) * transform.forward;
 
 		RaycastHit hit;
-		if (Physics.Raycast(detector.position, direction, out hit, ignoreLayers.value)) {
+        if (Physics.Raycast(detector.position, direction, out hit, Mathf.Infinity, walls)) {
 			return hit.distance;
 		}
 
 		return -1;
 	}
+
+    public float GetDistanceToCar(float deg) {
+        Vector3 direction = Quaternion.AngleAxis(deg, transform.up) * transform.forward;
+
+        RaycastHit hit;
+        if (Physics.Raycast(detector.position, direction, out hit, Mathf.Infinity, cars)) {
+            return hit.distance;
+        }
+
+        return -1;
+    }
+
+    public bool CarInFront() {
+        float distRight = GetDistanceToCar(30);
+
+        float distLeft = GetDistanceToCar(-30);
+
+        float dist = GetDistanceToCar(0);
+
+        return (distRight > 0 && distRight < 15) || (distLeft > 0 && distLeft < 15) || (dist > 0 && dist < 15);
+    }
+
+    public bool CarOnRight() {
+        float distFront = GetDistanceToCar(75);
+
+        float distBack = GetDistanceToCar(105);
+
+        return (distFront > 0 && distFront < 15) || (distBack > 0 && distBack < 15);
+    }
+
+    public bool CarOnLeft() {
+        float distFront = GetDistanceToCar(-75);
+
+        float distBack = GetDistanceToCar(-105);
+
+        return (distFront > 0 && distFront < 15) || (distBack > 0 && distBack < 15);
+    }
 
 	public float GetSpeed() {
 		return rigidbody.velocity.magnitude;
