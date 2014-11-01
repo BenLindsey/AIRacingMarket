0ed01b806d555ff2456e8ea6c18172951b744902
Christophe Steininger <christophe.steinininger@gmail.com>
2014-11-01 19:20:26 +0000

    Add skidding sound effect.

--- AIRacing/Assets/Plugins/CarSounds.cs
+++ AIRacing/Assets/Plugins/CarSounds.cs
@@ -4,17 +4,25 @@ using System.Collections;
 public class CarSounds : MonoBehaviour {
 
     public AudioClip engineSound;
+    public AudioClip skidSound;
 
-    private AudioSource engine;
+    private AudioSource engineSource;
+    private AudioSource skidSource;
 
 	// Use this for initialization
 	void Start () {
 
-        engine = gameObject.AddComponent<AudioSource>();
-        engine.loop = true;
-        engine.clip = engineSound;
-        engine.volume = 0;
-        engine.Play();
+        engineSource = gameObject.AddComponent<AudioSource>();
+        engineSource.loop = true;
+        engineSource.clip = engineSound;
+        engineSource.volume = 0;
+        engineSource.Play();
+
+        skidSource = gameObject.AddComponent<AudioSource>();
+        skidSource.loop = true;
+        skidSource.clip = skidSound;
+        skidSource.volume = 0;
+        skidSource.Play();
 	}
 	
 	// Update is called once per frame
@@ -22,6 +30,22 @@ public class CarSounds : MonoBehaviour {
 
         // Increase engine noise with speed.
         // TODO: Divide by a meaningful number.
-        engine.volume = Mathf.Clamp01(rigidbody.velocity.magnitude / 100f);
+        engineSource.volume = Mathf.Clamp01(rigidbody.velocity.magnitude / 200f);
+
+        skidSource.volume = (isSkidding()) ? 1 : 0;
 	}
+
+    private bool isSkidding() {
+        foreach (WheelCollider collider
+            in gameObject.GetComponentsInChildren<WheelCollider>()) {
+
+            WheelHit hit;
+            if (collider.GetGroundHit(out hit)
+                && (hit.forwardSlip > 2 || hit.sidewaysSlip > 2)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
