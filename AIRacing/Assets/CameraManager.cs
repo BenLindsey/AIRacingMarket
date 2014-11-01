89780cfc15f59d2876725136c868e32f8088ae69
Christophe Steininger <christophe.steinininger@gmail.com>
2014-11-01 18:54:26 +0000

    Add a simple engine sound to all cars which gets louder with the speed of the car.

--- AIRacing/Assets/CameraManager.cs
+++ AIRacing/Assets/CameraManager.cs
@@ -14,9 +14,17 @@ public class CameraManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	    if (Input.GetButtonDown("CameraToggle")) {
+
+            // Disable the current camera and destroy its audio listener. Unity
+            // will complain about multiple audio listeners even if only one is
+            // enabled.
             cameras[activeCamera].enabled = false;
+            Destroy(cameras[activeCamera].gameObject.GetComponent<AudioListener>());
+
+            // Activate the new camera and create an audio listener 
             activeCamera = (activeCamera + 1) % cameras.Count;
             cameras[activeCamera].enabled = true;
+            cameras[activeCamera].gameObject.AddComponent<AudioListener>();
         }
 	}
 
@@ -25,6 +33,9 @@ public class CameraManager : MonoBehaviour {
 
         for (int i = 0; i < cameras.Count; i++) {
             cameras[i].enabled = (i == activeCamera);
+            if (i == activeCamera) {
+                cameras[activeCamera].gameObject.AddComponent<AudioListener>();
+            }
         }
     }
 }
---
+++ AIRacing/Assets/Plugins/CarSounds.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarSounds : MonoBehaviour {
+
+    public AudioClip engineSound;
+
+    private AudioSource engine;
+
+	// Use this for initialization
+	void Start () {
+
+        engine = gameObject.AddComponent<AudioSource>();
+        engine.loop = true;
+        engine.clip = engineSound;
+        engine.volume = 0;
+        engine.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Increase engine noise with speed.
+        // TODO: Divide by a meaningful number.
+        engine.volume = Mathf.Clamp01(rigidbody.velocity.magnitude / 100f);
+	}
+}
