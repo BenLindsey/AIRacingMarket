14a10258162d0659dc5bf2d3ea26b51fe470c619
Jaime Lennox <jaimelennoxster@gmail.com>
2014-11-03 10:49:24 +0000

    Exclamation!

--- AIRacingNode/routes/index.js
+++ AIRacingNode/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.render('index', { title: 'AI Racing Market' });
+    res.render('index', { title: 'AI Racing Market!!!' });
 });
 
 module.exports = router;
