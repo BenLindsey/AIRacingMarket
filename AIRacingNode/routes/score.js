0869734707cd716c8fa9597b3a7f5adb8818b8ff
Louis Mackie <louis.mackie15@gmail.com>
2014-11-12 15:05:34 +0000

    Score route added

---
+++ AIRacingNode/routes/score.js
@@ -0,0 +1,23 @@
+var express = require ('express');
+var router = express.Router();
+
+router.post('/', function (req, res) {
+    var collection = req.db.get('scorecollection');
+
+    collection.insert({
+        "first"  : req.body.first,
+        "second" : req.body.second
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("It broke dingus");
+        }
+        else {
+            // If it worked, idk
+            res.location("leaderboard");
+            res.redirect("leaderboard");
+        }
+    });
+});
+
+module.exports = router; 
