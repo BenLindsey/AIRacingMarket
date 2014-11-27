eefa7e69dc8af601ad6fe2a80f25729bc5ba8f1f
Louis Mackie <louis.mackie15@gmail.com>
2014-11-27 16:26:30 +0000

    misspelling

--- AIRacingNode/routes/score.js
+++ AIRacingNode/routes/score.js
@@ -33,16 +33,14 @@ router.post('/', function (req, res) {
                   rolds[player] = 1400;
                   rxs[player] = 1400;
               } else {
-                  rolds[player] = doc.ranking;
-                  rxs[player] = doc.ranking;
-                  console.log("player: " + player + "ranking: " + rolds[player]);
+                  rolds[player] = doc.rating;
+                  rxs[player] = doc.rating;
               }
               j++; 
               if (j==req.body.players) {
                   for (x = 0; x < req.body.players; x++) {
                     for (y = 0; y < req.body.players; y++) {
                       if (x==y) continue;
-                      console.log("x: " + x + " ranking: " + rxs[x] + " " + rolds[x]);
                        rxs[x] = rX(rxs[x], calcW(x, y), eX(rolds[x], rolds[y]));
                       }
                     collection.update({
