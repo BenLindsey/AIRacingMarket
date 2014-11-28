9cb91874222326bf9ce877258a7f775d40f06ab4
Louis Mackie <louis.mackie15@gmail.com>
2014-11-28 15:18:34 +0000

    leaderboard complete

--- AIRacingNode/routes/leaderboard.js
+++ AIRacingNode/routes/leaderboard.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res) {
     var collection = req.db.get('scores');
 
-    collection.find({}, {sort : { rating : 1 }}, function(e, docs) {
+    collection.find({}, {sort : { rating : -1 }}, function(e, docs) {
         res.render('leaderboard', {
             "leaderboard" : docs
         });
