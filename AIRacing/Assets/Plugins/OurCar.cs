f92a2598d6a85dad8404abd4ab0ef5a387a407f0
Jaime Lennox <jaimelennoxster@gmail.com>
2014-12-03 02:08:02 +0000

    TOO MUCH FORCE NEEDS MORE JARJAR

--- AIRacing/Assets/Plugins/OurCar.cs
+++ AIRacing/Assets/Plugins/OurCar.cs
@@ -241,7 +241,7 @@ public class OurCar : MonoBehaviour {
         Debug.Log("Forward vector: " + rigidbody.transform.forward);
 
         if (boostCooldown == 0) {
-            rigidbody.velocity += rigidbody.transform.forward * 100;
+            rigidbody.velocity += rigidbody.transform.forward * 30;
             boostCooldown = MAX_BOOST;
         }
         
