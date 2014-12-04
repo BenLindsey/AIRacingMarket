1f1d5d387628838fc56d595642ca4f07e9338fff
Ben Lindsey <ben.lindsey12@imperial.ac.uk>
2014-12-04 17:21:52 +0000

    Added /tournment/next

---
+++ AIRacingNode/routes/tournament.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET next game */
+router.get('/next', function(req, res) {
+    var collection = req.db.get('scriptcollection');
+
+    collection.find({}, {sort : { scriptName : 1 }}, function(e, docs) {
+        var scriptsArray = [];
+        for(var doc in docs) {
+            scriptsArray.push(doc.scriptName);
+        }
+
+        if(scriptsArray.length < 4) {
+            res.send("There must be atleast 4 scripts in the database");
+            return;
+        }
+         
+        //todo levelname/carname
+        var url = "/webbuild?levelname=OvalTrack"
+                + "&carname=Catamount"
+
+        for (var ch in ["A", "B", "C", "D"]) {
+            var index =  Math.floor(Math.random() * scriptsArray.length);        
+            url += "&scripts[" + ch + "]=" + scriptsArray[index];
+            array.splice(index, 1);
+        }
+
+        console.log("Redirecting user to: " + url);
+        res.location(url);
+        res.redirect(url);
+    });
+});
+
+function isLoggedIn(req, res, next) {
+
+    req.session.redirect = '/tournament';
+
+    // if user is authenticated in the session, pass to GET/POST handlers
+    if (req.isAuthenticated()) {
+        return next();
+    }
+
+    // if they aren't redirect them to the login page
+    res.redirect('/login');
+}
+
+module.exports = router;
