b1bc2d5e4e9508091b781792f12088bf6e59dc59
Ben Lindsey <ben.lindsey12@imperial.ac.uk>
2014-12-04 17:30:53 +0000

    added logs

--- AIRacingNode/routes/tournament.js
+++ AIRacingNode/routes/tournament.js
@@ -5,11 +5,15 @@ var router = express.Router();
 router.get('/next', function(req, res) {
     var collection = req.db.get('scriptcollection');
 
+    console.log("Next!");
+
     collection.find({}, {sort : { scriptName : 1 }}, function(e, docs) {
+        console.log("Found!");
         var scriptsArray = [];
-        for(var doc in docs) {
+        for (var doc in docs) {
             scriptsArray.push(doc.scriptName);
         }
+        console.log("build array of size " + scriptsArray.length);
 
         if(scriptsArray.length < 4) {
             res.send("There must be atleast 4 scripts in the database");
@@ -21,7 +25,8 @@ router.get('/next', function(req, res) {
                 + "&carname=Catamount"
 
         for (var ch in ["A", "B", "C", "D"]) {
-            var index =  Math.floor(Math.random() * scriptsArray.length);        
+            var index =  Math.floor(Math.random() * scriptsArray.length);    
+            console.log("chose script: " + scriptsArray[index]);    
             url += "&scripts[" + ch + "]=" + scriptsArray[index];
             array.splice(index, 1);
         }
