676b3b95008253cc397259166508ff2b69079911
Jaime Lennox <jaimelennoxster@gmail.com>
2015-01-12 21:26:58 +0000

    Fixed variable usage in jade.

--- AIRacingNode/routes/results.js
+++ AIRacingNode/routes/results.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET user profile. */
 router.get('/', function(req, res) {
-    res.render('results');
+    res.render('results', {scripts : req.query.scripts});
 });
 
 module.exports = router;
