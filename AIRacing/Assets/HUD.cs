d9249729bdf50cb6da2f509d908357ba8f08c6e2
Christophe Steininger <christophe.steininger@gmail.com>
2015-01-13 19:12:37 +0000

    Draw car labels in the correct order.

--- AIRacing/Assets/HUD.cs
+++ AIRacing/Assets/HUD.cs
@@ -159,7 +159,7 @@ public class HUD : MonoBehaviour {
 
         // Draw the lap number for the current car.
         if (raceMode != RaceMode.Test) {
-            string text = "Lap " + (currentState.lap + 1);
+            string text = "Lap " + (currentState.lap + 1) + " / " + LAPS_IN_RACE;
             float textWidth = style.CalcSize(new GUIContent(text)).x;
             DrawOutlinedText(new Rect(carCamera.pixelWidth - textWidth - 10, y, textWidth, 100),
                 text, Color.white, style);
@@ -179,17 +179,41 @@ public class HUD : MonoBehaviour {
             DrawOutlinedText(ordinalRect, ordinals[i], Color.white, ordinalStyle);
         }
 
+        // Draw the car names next to the place names in the correct order.
+        float[] distancesToCamera = new float[carStates.Length];
         for (int i = 0; i < carStates.Length; i++) {
+            // Calculate the distance between the camera and the car for the next loop.
+            distancesToCamera[i] = (carStates[i].car.transform.position
+                - carCamera.transform.position).magnitude;
+
             int position = carStates[i].position + 1;
             Color textColor = (carStates[i].position < carsFinished)
                 ? carFinishedColor
                 : ((carStates[i].HasCarStopped) ? carTimedOutColor : carRacingColor);
 
-            // Draw the car's name over its model if it's in front of the camera.
-            Vector3 screenPosition = carCamera.WorldToScreenPoint(
-                carStates[i].car.transform.position + 2.5f * Vector3.up);
-            if (screenPosition.z > 0) {
+            // Draw the car's row on the leaderboard.
+            DrawOutlinedText(new Rect(nameLocation + 2, y + yDiff * (position - 1), 200, 100),
+                carStates[i].name, textColor, style);
+        }
+        y += yDiff * carStates.Length;
 
+        // Draw the labels above the cars. Draw the labels furthest away first.
+        for (int step = 0; step < carStates.Length; step++) {
+            // Find the furthest car from the camera without a label
+            int maxIndex = 0;
+            for (int i = 1; i < carStates.Length; i++) {
+                if (distancesToCamera[i] > distancesToCamera[maxIndex]) {
+                    maxIndex = i;
+                }
+            }
+
+            // Remember not to label this car again in future iterations.
+            distancesToCamera[maxIndex] = -1;
+
+            // Draw the label over this car if it is in front of the camera.
+            Vector3 screenPosition = carCamera.WorldToScreenPoint(
+                carStates[maxIndex].car.transform.position + 2.5f * Vector3.up);
+            if (screenPosition.z > 0) {
                 // Use 'minSize' as the font size if the car is further than 'farZ' from the
                 // camera. If the car is closer than 'nearZ' use 'maxSize', otherwise linearly
                 // scale the font size between these points.
@@ -201,18 +225,13 @@ public class HUD : MonoBehaviour {
                 style.fontSize = (int)Mathf.Clamp((minSize - maxSize) / (farZ - nearZ)
                     * (screenPosition.z - nearZ) + maxSize, minSize, maxSize);
 
-                float xOffset = style.CalcSize(new GUIContent(carStates[i].name)).x / 2;
+                float xOffset = style.CalcSize(new GUIContent(carStates[maxIndex].name)).x / 2;
                 DrawOutlinedText(new Rect(screenPosition.x - xOffset,
-                    carCamera.pixelHeight - screenPosition.y, 200, 100), carStates[i].name,
-                    Color.white, style);
+                    carCamera.pixelHeight - screenPosition.y, 200, 100),
+                    carStates[maxIndex].name, Color.white, style);
                 style.fontSize = originalFontSize;
             }
-
-            // Draw the car's row on the leaderboard.
-            DrawOutlinedText(new Rect(nameLocation + 2, y + yDiff * (position - 1), 200, 100),
-                carStates[i].name, textColor, style);
         }
-        y += yDiff * carStates.Length;
     }
 
     private void DrawOutlinedText(Rect location, string text, Color textColor, GUIStyle style) {
